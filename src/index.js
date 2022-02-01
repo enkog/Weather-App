ca757e980a1b3729d3bacd76d7a25ef5a6f3d536
enkog <nkirukaogu@gmail.com>
2022-02-01 22:45:26 +0100

    Destructure api data and display them on the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { DateTime } from './luxon.js';
-import axios from 'axios';
 import './style.css';
+import axios from 'axios';
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const apiKey = 'e9391e1ce68c9e71ee8fc43b93ea9697';
@@ -9,11 +9,31 @@ const apiKey = 'e9391e1ce68c9e71ee8fc43b93ea9697';
 const fetchWeather = async (city = 'Lagos') => {
     try {
         const response = await axios.get(`${baseUrl}weather?q=${city}&units=metric&appid=${apiKey}`);
-        console.log(response.data);
+        displayWeather(response.data);
     } catch (error) {
-        document.querySelector('.current-temp').innerHTML = `<p class="no-data">No data found for this city !!!</p>`;
+        document.querySelector('.current-temp').innerHTML = 
+        `
+        <p class="no-data">No data found for this city !!!</p>
+        `;
         document.querySelector('.temp-metrics').classList.add('hidden');
     }  
 }
+
+const displayWeather = (data) => {
+    const {name} = data;
+    const {icon, description} = data.weather[0];
+    const {country} = data.sys;
+    const {humidity, temp, pressure} = data.main;
+    const {speed} = data.wind;
+
+    document.querySelector('.city').textContent = `${name}, `;
+    document.querySelector('.country').textContent = country;
+    document.querySelector('.main-temp').textContent = `${temp}°C`;
+    document.querySelector('.weather-icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector('.humidity').textContent = `${humidity}%`;
+    document.querySelector('.wind').textContent = `${speed} km/h`;
+    document.querySelector('.description').textContent = description;
+    document.querySelector('.pressure').textContent = `${pressure} hPa`;
+  }
 
 document.getElementById('date-time').textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED).toString();
