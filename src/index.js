162b629b291b8b730354950c38510d8a17bff7b7
enkog <nkirukaogu@gmail.com>
2022-02-01 07:21:49 +0100

    Add current date and time

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import _ from 'lodash';
+import { DateTime } from './luxon.js';
 import './style.css';
+
+document.getElementById('date-time').textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED).toString();
