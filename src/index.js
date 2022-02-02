38fea396cf606ee5f4ba3ac88d68e183ff9fd630
enkog <nkirukaogu@gmail.com>
2022-02-02 10:59:26 +0100

    Add a click event for the search bars

--- src/index.js
+++ src/index.js
@@ -13,31 +13,43 @@ const fetchWeather = async (city = 'Lagos') => {
         displayWeather(response.data);
     } catch (error) {
         $('.current-temp').html(`<p class="no-data">No data found for this city !!!</p>`);
+        $('.min-max').html(`<p class="no-data">No data found for this city !!!</p>`);
         $('.temp-metrics').addClass('hidden');
-    }  
+    }
 }
 
 const displayWeather = (data) => {
     const {name} = data;
     const {icon, description} = data.weather[0];
     const {country} = data.sys;
-    const {humidity, temp, pressure} = data.main;
+    const {humidity, temp, pressure, temp_min, temp_max} = data.main;
     const {speed} = data.wind;
 
     $(".city").text(`${name}, `);
     $(".country").text(country);
-    $('.main-temp').text (`${temp}°C`);
+    $('.main-temp').text(`${temp}°C`);
     $('.weather-icon').attr("src", `https://openweathermap.org/img/wn/${icon}@2x.png`);
-    $('.humidity').text (`${humidity}%`);
-    $('.wind').text (`${speed} km/h`);
-    $('.description').text (description);
-    $('.pressure').text (`${pressure} hPa`);
+    $('.humidity').text(`${humidity}%`);
+    $('.wind').text(`${speed} km/h`);
+    $('.description').text(description);
+    $('.pressure').text(`${pressure} hPa`);
+
+    $(".search-btn2").on("click", (e) => {
+        e.preventDefault();
+        const country = $(".search-weather2").val();
+        fetchWeather(country);
+        $('.coldest-weather').text(temp_min);
+        $('.warmest-weather').text(temp_max);
+        $('.current-temp').addClass('hidden');
+        $('.temp-metrics').addClass('hidden');
+        $('.min-max').removeClass('hidden');
+    });
 }
 
 $(".search-btn1").on("click", (e) => {
     e.preventDefault();
-    const country = $(".search-weather").val();
-    fetchWeather(country);
+    const country = $(".search-weather1").val();
+    fetchWeather(country); 
 });
 
 $('#date-time').text(DateTime.now().toLocaleString(DateTime.DATETIME_MED).toString());
