7b4b81d7d665d70f727191818cddb7dcd0a2724c
enkog <nkirukaogu@gmail.com>
2022-02-03 10:54:37 +0100

    Display minimum and maximum temperature of a country or city

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import _ from 'lodash';
 import $ from 'jquery';
 import { DateTime } from './luxon.js';
+import { fetchWeather } from './api.js';
 import './style.css';
-import axios from 'axios';
 
-const baseUrl = 'https://api.openweathermap.org/data/2.5/';
-const apiKey = 'e9391e1ce68c9e71ee8fc43b93ea9697';
-
-const fetchWeather = async (city = 'Lagos') => {
-    try {
-        const response = await axios.get(`${baseUrl}weather?q=${city}&units=metric&appid=${apiKey}`);
-        displayWeather(response.data);
-    } catch (error) {
-        $('.current-temp').html(`<p class="no-data">No data found for this city !!!</p>`);
-        $('.min-max').html(`<p class="no-data">No data found for this city !!!</p>`);
-        $('.temp-metrics').addClass('hidden');
-    }
-}
 
 const displayWeather = (data) => {
     const {name} = data;
     const {icon, description} = data.weather[0];
     const {country} = data.sys;
-    const {humidity, temp, pressure, temp_min, temp_max} = data.main;
+    const {humidity, temp, pressure} = data.main;
     const {speed} = data.wind;
 
     $(".city").text(`${name}, `);
@@ -33,28 +20,43 @@ const displayWeather = (data) => {
     $('.wind').text(`${speed} km/h`);
     $('.description').text(description);
     $('.pressure').text(`${pressure} hPa`);
-
-    $(".search-btn2").on("click", (e) => {
-        e.preventDefault();
-        const country = $(".search-weather2").val();
-        fetchWeather(country);
-        $('.coldest-weather').text(temp_min);
-        $('.warmest-weather').text(temp_max);
-        $('.current-temp').addClass('hidden');
-        $('.temp-metrics').addClass('hidden');
-        $('.min-max').removeClass('hidden');
-    });
 }
+
+const getWeather = async (city) => {
+    try {
+      const response = await fetchWeather(city);
+      displayWeather(response);
+    } catch (error) {
+      console.log('No data found for this city');
+    }
+  };
 
 $(".search-btn1").on("click", (e) => {
     e.preventDefault();
-    const country = $(".search-weather1").val();
-    fetchWeather(country); 
+    const term = $(".search-weather1").val();
+    $('.search-weather1').val("");
+    getWeather(term); 
+});
+
+$(".search-btn2").on("click", async (e) => {
+    e.preventDefault();
+    const term = $(".search-weather2").val();
+    $('.search-weather2').val("");
+    const data = await fetchWeather(term);
+    const {temp_min, temp_max, temp} = data.main;
+    const {name} = data;
+
+    $('.country-temp').removeClass('hidden');
+    $('.city-temp').text(`${name}, ${temp}°C`);
+    $('.current-temp').addClass('hidden');
+    $('.temp-metrics').addClass('hidden');
+    $('.coldest-weather').html(`<h2 class="cw-header">Coldest Temperature</h2> <p class="cw-p">${temp_min}</p>`);
+    $('.warmest-weather').html(`<h2 class="cw-header">Warmest Temperature</h2> <p class="cw-p">${temp_max}</p>`);
 });
 
 $('#date-time').text(DateTime.now().toLocaleString(DateTime.DATETIME_MED).toString());
 
 $(window).on('load', (e) => {
     e.preventDefault();
-    fetchWeather();
+    getWeather();
 });
